1cabdc10b4aa12c0ae016918a36f47de82b4501c
unknown <dicksonevergreenido@gmail.com>
2020-08-04 01:57:34 +0100

    fixing

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -24,98 +24,190 @@
       </div>
     </div>
 
-    <div class="hero-second text-center">
+    <div class="hero-second text-center mb-5">
       <h1>Dance, at a Glance</h1>
       <p>Get all the pieces you need, and none of the ones that you don’t. We’ve crafted a single dead-simple service for you.</p>
 
-<div class="container">
-<div class="icons-area row m-auto">
+      <div class="container">
+        <div class="icons-area row m-auto">
+          <div class="col-lg-4">
+            <svg
+              width="100"
+              height="100"
+              viewBox="0 0 100 100"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="Glance_icon__jI9Gv Glance_icon__jI9Gv"
+            >
+              <rect width="100" height="100" rx="24" fill="#E0D3A7" fill-opacity="0.35" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M29.4166 57.6H26L29.75 48L30.5833 54.3999H33.9999L30.2499 63.9999L29.4166 57.6Z"
+                fill="#49464F"
+              />
+              <circle cx="30" cy="56" r="12" stroke="#49464F" stroke-width="4" />
+              <circle cx="70" cy="56" r="12" stroke="#49464F" stroke-width="4" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M50.4142 38H52.2811C53.3279 38 54.3331 38.4104 55.0808 39.1431L62.1214 46.0428L70.3284 54.0856L67.5 56.914L50.4142 39.8282L39.5 50.5L36.6716 47.6716L45.7582 39.0916C46.5007 38.3906 47.4832 38 48.5044 38H50.4142Z"
+                fill="#49464F"
+              />
+              <path d="M29.5 32H50.5" stroke="#49464F" stroke-width="4" />
+              <path
+                d="M78.0001 31.4209C73.1645 34.8593 66.6347 34.8593 61.7991 31.4209"
+                stroke="#49464F"
+                stroke-width="4"
+              />
+            </svg>
+            <p class="text-iconx">An e-bike</p>
+            <p class="text-icon">
+              The ultimate urban
+              <br />electric bike
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <svg
+              width="100"
+              height="100"
+              viewBox="0 0 100 100"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="Glance_icon__jI9Gv Glance_icon__jI9Gv"
+            >
+              <rect width="100" height="100" fill="#FFC9BB" fill-opacity="0.35" />
+              <path
+                d="M77.0938 47.2L72.25 52.4V50C72.25 36.8 61.7875 26 49 26C42.8 26 36.9875 28.4 32.5312 33L35.2437 35.8C38.925 32 43.7688 30 49 30C59.6562 30 68.375 39 68.375 50V52.4L63.5312 47.4L60.8188 50L70.3125 60L80 50L77.0938 47.2Z"
+                fill="#49464F"
+              />
+              <path
+                d="M49 70C38.3438 70 29.625 61 29.625 50V47.6L34.4688 52.6L37.1812 49.8L27.6875 40L18 49.8L20.7125 52.6L25.75 47.6V50C25.75 63.2 36.2125 74 49 74C55.2 74 61.0125 71.6 65.4688 67L62.7563 64.2C59.075 68 54.2313 70 49 70Z"
+                fill="#49464F"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M51 47C51 47.5523 50.5523 48 50 48H42C41.4477 48 41 47.5523 41 47V47C41 46.4477 41.4477 46 42 46H50C50.5523 46 51 46.4477 51 47V47Z"
+                fill="#49464F"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M51 51C51 51.5523 50.5523 52 50 52H42C41.4477 52 41 51.5523 41 51V51C41 50.4477 41.4477 50 42 50H50C50.5523 50 51 50.4477 51 51V51Z"
+                fill="#49464F"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M43 47.5C43 43.3579 46.3579 40 50.5 40C52.4884 40 54.296 40.7738 55.6382 42.0366L53.5153 44.1596C52.7173 43.4388 51.6599 43 50.5 43C48.0147 43 46 45.0147 46 47.5V51.5C46 53.9853 48.0147 56 50.5 56C51.5767 56 52.5652 55.6218 53.3397 54.9911L55.4677 57.119C54.1447 58.2895 52.4053 59 50.5 59C46.3579 59 43 55.6421 43 51.5V47.5Z"
+                fill="#49464F"
+              />
+            </svg>
+            <p class="text-iconx">A subscription</p>
+            <p class="text-icon">
+              Pay monthly, e-bike
+              <br />&amp; all services included.
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <svg
+              width="100"
+              height="100"
+              viewBox="0 0 100 100"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="Glance_icon__jI9Gv Glance_icon__jI9Gv"
+            >
+              <rect width="100" height="100" fill="#A7E0DB" fill-opacity="0.35" />
+              <path d="M44 64H56V68H44V64Z" fill="#49464F" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M61.3273 25.2L64.9441 28.8C65.7479 29.6 66.1497 30.6 65.9488 31.6V68.4C65.9488 69.4 65.5469 70.4 64.7432 71.2L61.1263 74.8C60.3226 75.6 59.3179 76 58.3132 76H41.6356C40.6309 76 39.6262 75.6 38.8225 74.8L35.2056 71.2C34.4019 70.4 34 69.4 34 68.4V31.6C34 30.6 34.4019 29.6 35.2056 28.8L38.8225 25.2C39.6262 24.4 40.6309 24 41.6356 24H58.5142C59.5189 24 60.5235 24.4 61.3273 25.2ZM38.0187 72H62.131V28H38.0187V72Z"
+                fill="#49464F"
+              />
+            </svg>
+            <p class="text-iconx">An app</p>
+            <p class="text-icon">
+              Turn your phone into
+              <br />a powerful bike dashboard
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-<div class="col-lg-4">
+    <div class="hero-third hero-container">
+      <div class="row container">
+        <div class="col-lg-6 mx-5">
+          <h1>The Ultimate Urban Electric Bike.</h1>
 
-  <svg
-        width="100"
-        height="100"
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="Glance_icon__jI9Gv Glance_icon__jI9Gv"
-      >
-        <rect width="100" height="100" rx="24" fill="#E0D3A7" fill-opacity="0.35" />
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M29.4166 57.6H26L29.75 48L30.5833 54.3999H33.9999L30.2499 63.9999L29.4166 57.6Z"
-          fill="#49464F"
-        />
-        <circle cx="30" cy="56" r="12" stroke="#49464F" stroke-width="4" />
-        <circle cx="70" cy="56" r="12" stroke="#49464F" stroke-width="4" />
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M50.4142 38H52.2811C53.3279 38 54.3331 38.4104 55.0808 39.1431L62.1214 46.0428L70.3284 54.0856L67.5 56.914L50.4142 39.8282L39.5 50.5L36.6716 47.6716L45.7582 39.0916C46.5007 38.3906 47.4832 38 48.5044 38H50.4142Z"
-          fill="#49464F"
-        />
-        <path d="M29.5 32H50.5" stroke="#49464F" stroke-width="4" />
-        <path
-          d="M78.0001 31.4209C73.1645 34.8593 66.6347 34.8593 61.7991 31.4209"
-          stroke="#49464F"
-          stroke-width="4"
-        />
-      </svg>
-      <p class="text-iconx">An e-bike</p>
-      <p class="text-icon">The ultimate urban <br>electric bike</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat temporibus quo nostrum asperiores quibusdam. Repudiandae illum, veritatis repellat omnis earum officia, quos sit ullam dolorum rerum incidunt, fugit temporibus inventore?Quasi, quas praesentium doloremque quisquam minima maxime sapiente quos quibusdam suscipit debitis quod rem illum eveniet ipsum corporis accusantium autem ipsam numquam ea cupiditate delectus vel? Vero rerum labore iure.</p>
 
-</div>
-<div class="col-lg-4">
+          <button class="btn btnx">Request an invite</button>
+        </div>
+        <div class="col-lg-6">
+          <img src />
+        </div>
+      </div>
 
-   <svg
-        width="100"
-        height="100"
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="Glance_icon__jI9Gv Glance_icon__jI9Gv"
-      >
-        <rect width="100" height="100" fill="#FFC9BB" fill-opacity="0.35" />
-        <path
-          d="M77.0938 47.2L72.25 52.4V50C72.25 36.8 61.7875 26 49 26C42.8 26 36.9875 28.4 32.5312 33L35.2437 35.8C38.925 32 43.7688 30 49 30C59.6562 30 68.375 39 68.375 50V52.4L63.5312 47.4L60.8188 50L70.3125 60L80 50L77.0938 47.2Z"
-          fill="#49464F"
-        />
-        <path
-          d="M49 70C38.3438 70 29.625 61 29.625 50V47.6L34.4688 52.6L37.1812 49.8L27.6875 40L18 49.8L20.7125 52.6L25.75 47.6V50C25.75 63.2 36.2125 74 49 74C55.2 74 61.0125 71.6 65.4688 67L62.7563 64.2C59.075 68 54.2313 70 49 70Z"
-          fill="#49464F"
-        />
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M51 47C51 47.5523 50.5523 48 50 48H42C41.4477 48 41 47.5523 41 47V47C41 46.4477 41.4477 46 42 46H50C50.5523 46 51 46.4477 51 47V47Z"
-          fill="#49464F"
-        />
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M51 51C51 51.5523 50.5523 52 50 52H42C41.4477 52 41 51.5523 41 51V51C41 50.4477 41.4477 50 42 50H50C50.5523 50 51 50.4477 51 51V51Z"
-          fill="#49464F"
-        />
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M43 47.5C43 43.3579 46.3579 40 50.5 40C52.4884 40 54.296 40.7738 55.6382 42.0366L53.5153 44.1596C52.7173 43.4388 51.6599 43 50.5 43C48.0147 43 46 45.0147 46 47.5V51.5C46 53.9853 48.0147 56 50.5 56C51.5767 56 52.5652 55.6218 53.3397 54.9911L55.4677 57.119C54.1447 58.2895 52.4053 59 50.5 59C46.3579 59 43 55.6421 43 51.5V47.5Z"
-          fill="#49464F"
-        />
-      </svg>
-      <p class="text-iconx">A subscription</p>
-      <p class="text-icon">Pay monthly, e-bike &amp; all services included.</p>
-</div>
-<div class="col-lg-4">
-  <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" class="Glance_icon__jI9Gv Glance_icon__jI9Gv"><rect width="100" height="100" fill="#A7E0DB" fill-opacity="0.35"></rect><path d="M44 64H56V68H44V64Z" fill="#49464F"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M61.3273 25.2L64.9441 28.8C65.7479 29.6 66.1497 30.6 65.9488 31.6V68.4C65.9488 69.4 65.5469 70.4 64.7432 71.2L61.1263 74.8C60.3226 75.6 59.3179 76 58.3132 76H41.6356C40.6309 76 39.6262 75.6 38.8225 74.8L35.2056 71.2C34.4019 70.4 34 69.4 34 68.4V31.6C34 30.6 34.4019 29.6 35.2056 28.8L38.8225 25.2C39.6262 24.4 40.6309 24 41.6356 24H58.5142C59.5189 24 60.5235 24.4 61.3273 25.2ZM38.0187 72H62.131V28H38.0187V72Z" fill="#49464F"></path></svg><p class="text-iconx">An app</p><p class="text-icon">Turn your phone into a powerful bike dashboard</p>
-</div>
-
-</div>
-</div>
-      
-     
+      <div class="row container mt-5 mx-auto">
+        <div class="col-lg-6 col-md-6 text-center">
+          <svg
+            width="32"
+            height="20"
+            viewBox="0 0 32 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M16 0H6V2H16V0Z" fill="#49464F" />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M25 6C23.6 6 22.3 6.4 21.2 7.2L18.6 4.6C18.2 4.2 17.7 4 17.2 4H14.9C14.4 4 13.9 4.2 13.5 4.6L10.9 7.2C9.7 6.4 8.4 6 7 6C3.1 6 0 9.1 0 13C0 16.9 3.1 20 7 20C10.9 20 14 16.9 14 13C14 11.3 13.4 9.7 12.3 8.5L16 4.8L19.7 8.5C18.7 9.7 18 11.3 18 13C18 16.9 21.1 20 25 20C28.9 20 32 16.9 32 13C32 9.1 28.9 6 25 6ZM12 13C12 15.8 9.8 18 7 18C4.2 18 2 15.8 2 13C2 10.2 4.2 8 7 8C7.9 8 8.7 8.2 9.4 8.6L6 12L7.4 13.4L10.9 9.9C11.6 10.8 12 11.8 12 13ZM25 18C22.2 18 20 15.8 20 13C20 11.8 20.4 10.8 21.1 9.9L24.6 13.4L26 12L22.6 8.6C23.3 8.2 24.1 8 25 8C27.8 8 30 10.2 30 13C30 15.8 27.8 18 25 18Z"
+              fill="#49464F"
+            />
+            <path
+              d="M28.5 0L29.9 1.4C28.6 2.8 26.6 3.4 24.9 3.4C23.2 3.4 21.4 2.8 20 1.4L21.4 0C23.4 1.9 26.6 1.9 28.5 0Z"
+              fill="#49464F"
+            />
+          </svg>
+          <p class="text-iconx">Electric</p>
+          <p>
+            Go further, arrive sooner,
+            <br />with no sweat.
+          </p>
+        </div>
+      <div class="col-lg-6 col-md-6 text-center">
+          <svg
+            width="29"
+            height="28"
+            viewBox="0 0 29 28"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4.94264 4.1C7.47559 1.5 10.9828 0 14.5873 0C22.0888 0 28.2263 6.3 28.2263 14H26.2779C26.2779 7.4 21.0171 2 14.5873 2C11.4699 2 8.54722 3.2 6.30654 5.5L4.94264 4.1Z"
+              fill="#49464F"
+            />
+            <path
+              d="M2.89682 14C2.89682 20.6 8.15756 26 14.5874 26C17.7048 26 20.6275 24.8 22.8682 22.5L24.2321 23.9C21.6991 26.5 18.1919 28 14.5874 28C7.08593 28 0.948395 21.7 0.948395 14H2.89682Z"
+              fill="#49464F"
+            />
+            <path
+              d="M23.3553 14C23.3553 9 19.4584 5 14.5874 5V7C18.3868 7 21.4068 10.1 21.4068 14H23.3553Z"
+              fill="#49464F"
+            />
+            <path
+              d="M14.5874 21V23C9.7163 23 5.81945 19 5.81945 14H7.76788C7.76788 17.9 10.7879 21 14.5874 21Z"
+              fill="#49464F"
+            />
+          </svg>
+          <p class="text-iconx">Sustainable</p>
+          <p>Healthy for you, healthy <br>for the environment.</p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
