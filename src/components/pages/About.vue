461605a8b2c68ad7898f70c35334ca0c9562eff8
unknown <dicksonevergreenido@gmail.com>
2020-08-06 16:07:01 +0100

    fixing commit time issues

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -3,34 +3,44 @@
     <h1 class="text-center mb-5 pb-5">Joyful movement. Sustainable Earth</h1>
 
     <div class="hero-container">
+      <div class="container mt-5 p-5">
+        <p>
+          <b>Run x</b> is a lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Dolore at consectetur optio harum mollitia alias! Voluptatem quis perferendis mollitia ullam obcaecati eum quisquam
+          pariatur velit quae? Nemo, eum. Architecto, voluptates.
+        </p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 mx-auto mt-5 ">
+          <p>
+            Dear Future Runner ,
+            <br />
+We believe that Run x is the only Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Odio, vitae omnis fuga quaerat eum,
+            harum cum nobis tempora quibusdam ipsa at nulla laborum numquam sit
+            soluta ducimus neque debitis? Ipsa sit amet consectetur adipisicing elit. Esse libero accusantium facere tenetur id nisi fugiat veritatis voluptates. Quaerat necessitatibus neque eos amet magni odio unde voluptas ipsa vitae sed?
+      Ad illum labore ea unde quia vel dolorum laboriosam reprehenderit odio blanditiis placeat fugiat iste corporis repellat quo corrupti, porro facilis expedita!
+         <br><br>
+         ido dickson evergreen
+         <br>
+         <span>CTO</span>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="image-areax container">
         
-        <div class="container mt-5 p-5">
-
-            <p><b> Run x </b> is a lorem ipsum dolor sit, amet consectetur adipisicing elit.
-         Dolore at consectetur optio harum mollitia alias! Voluptatem quis perferendis mollitia ullam obcaecati eum quisquam 
-         pariatur velit quae? Nemo, eum. Architecto, voluptates.</p>
-        </div>
+    
         </div>
 
+        <div class="career_area container">
 
-        <div class="row container">
+Our team is growing 
 
-
-            <div class="col-lg-4 mx-auto my-auto mt-auto mb-auto">
-
-
- <p> Dear Future Runner , <br/>
-
-we believe the Run x is the only Lorem ipsum dolor, sit amet 
-consectetur adipisicing elit. Odio, vitae omnis fuga quaerat eum,
- harum cum nobis tempora quibusdam ipsa at nulla laborum numquam sit
-  soluta ducimus neque debitis? Ipsa!
-
- </p>
-
-            </div>
         </div>
- 
- 
   </section>
 </template>
