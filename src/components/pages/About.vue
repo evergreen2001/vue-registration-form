98c6576238bdc3ecafc75b07b213144fa450ddf9
unknown <dicksonevergreenido@gmail.com>
2020-08-06 16:32:10 +0100

    about cmp ready

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -14,33 +14,36 @@
 
     <div class="container">
       <div class="row">
-        <div class="col-lg-6 mx-auto mt-5 ">
+        <div class="col-lg-6 mx-auto mt-5">
           <p>
             Dear Future Runner ,
-            <br />
-We believe that Run x is the only Lorem ipsum dolor, sit amet
+            <br />We believe that Run x is the only Lorem ipsum dolor, sit amet
             consectetur adipisicing elit. Odio, vitae omnis fuga quaerat eum,
             harum cum nobis tempora quibusdam ipsa at nulla laborum numquam sit
             soluta ducimus neque debitis? Ipsa sit amet consectetur adipisicing elit. Esse libero accusantium facere tenetur id nisi fugiat veritatis voluptates. Quaerat necessitatibus neque eos amet magni odio unde voluptas ipsa vitae sed?
-      Ad illum labore ea unde quia vel dolorum laboriosam reprehenderit odio blanditiis placeat fugiat iste corporis repellat quo corrupti, porro facilis expedita!
-         <br><br>
-         ido dickson evergreen
-         <br>
-         <span>CTO</span>
+            Ad illum labore ea unde quia vel dolorum laboriosam reprehenderit odio blanditiis placeat fugiat iste corporis repellat quo corrupti, porro facilis expedita!
+            <br />
+            <br />ido dickson evergreen
+            <br />
+            <span>CTO</span>
           </p>
         </div>
       </div>
     </div>
 
-    <div class="image-areax container">
-        
-    
-        </div>
+    <div class="image-areax container"></div>
 
-        <div class="career_area container">
+    <div class="career_area container text-white">
+      <p class="text-white">
+       <span class="brown_text"> Our team is growing.</span> Lorem ipsum dolor sit amet
+        harum vitae, odit eos porro quae cum. Corporis quisquam quam accusantium!consectetur adipisicing elit. Omnis, beatae. Modi atque veniam quidem, expedita
+        voluptates assumenda excepturi totam harum vitae, odit eos porro quae cum. Corporis quisquam quam accusantium!
+      </p>
 
-Our team is growing 
-
-        </div>
+      <i>
+       <span class="brown_text"> Check our Job Board</span>
+        <img src="../../assets/icon-arrow.svg" width="30" />
+      </i>
+    </div>
   </section>
 </template>
