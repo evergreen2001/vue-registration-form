f1079efecc303582698fac08c4aec7b4900c66f9
unknown <dicksonevergreenido@gmail.com>
2020-08-07 03:21:34 +0100

    on the about cmp

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,12 +1,36 @@
 <template>
-    <section class="container">
+  <section>
+    <h1 class="text-center mb-5 pb-5">Joyful movement. Sustainable Earth</h1>
 
-        <h1 class="text-center">
+    <div class="hero-container">
+        
+        <div class="container mt-5 p-5">
 
-    Joyful movement. Sustainable Earth
-    
-    
-    
-</h1>
-    </section>
+            <p><b> Run x </b> is a lorem ipsum dolor sit, amet consectetur adipisicing elit.
+         Dolore at consectetur optio harum mollitia alias! Voluptatem quis perferendis mollitia ullam obcaecati eum quisquam 
+         pariatur velit quae? Nemo, eum. Architecto, voluptates.</p>
+        </div>
+        </div>
+
+
+        <div class="row container">
+
+
+            <div class="col-lg-4 mx-auto my-auto mt-auto mb-auto">
+
+
+ <p> Dear Future Runner , <br/>
+
+we believe the Run x is the only Lorem ipsum dolor, sit amet 
+consectetur adipisicing elit. Odio, vitae omnis fuga quaerat eum,
+ harum cum nobis tempora quibusdam ipsa at nulla laborum numquam sit
+  soluta ducimus neque debitis? Ipsa!
+
+ </p>
+
+            </div>
+        </div>
+ 
+ 
+  </section>
 </template>
